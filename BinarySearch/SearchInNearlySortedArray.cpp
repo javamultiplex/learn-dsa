d8847b630c2349fe9c931a060acd460446d9a15b
Rohit Agarwal <programmingwithrohit@gmail.com>
2023-07-08 21:52:16 +0530

    added  new problem

---
+++ BinarySearch/SearchInNearlySortedArray.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+int binarySearch(vector<int> arr, int target)
+{
+    int start = 0;
+    int end = arr.size() - 1;
+    int mid = start + (end - start) / 2;
+    while (start <= end)
+    {
+        if (arr[mid] == target)
+        {
+            return mid;
+        }
+        if (mid - 1 >= start && arr[mid - 1] == target)
+        {
+            return mid - 1;
+        }
+        if (mid + 1 <= end && arr[mid + 1] == target)
+        {
+            return mid + 1;
+        }
+
+        if (arr[mid] < target)
+        {
+            start = mid + 2;
+        }
+        else
+        {
+            end = mid - 2;
+        }
+
+        mid = start + (end - start) / 2;
+    }
+    return -1;
+}
+int main()
+{
+    vector<int> arr{10, 3, 40, 20, 50, 80};
+    int target = 40;
+    int index = binarySearch(arr, target);
+    if (index == -1)
+    {
+        cout << "Element " << target << " not found";
+    }
+    else
+    {
+        cout << "Element " << target << " found at index " << index;
+    }
+}
